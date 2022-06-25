27b6231067ea260b70e9cf75b6411d362c42b0c4
AtulSikarwal <atulsikarwal5.com>
2022-06-25 10:43:11 +0530

    The code for create a BST And Show

---
+++ CreateShow/BinaryTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaraySearchTree
+{
+    public class BinarySearchTreeNode<K>
+    {
+        public K Key;
+        public BinarySearchTreeNode<K> Right;
+        public BinarySearchTreeNode<K> Left;
+
+
+        public BinarySearchTreeNode(K Key)
+        {
+            this.Key = Key;
+            Right = null;
+            Left = null;
+        }
+    }
+}
