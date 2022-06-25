955d138f22c8f2e47d7f4b9c59eb27be4226cdb5
AtulSikarwal <89919369+AtulSikarwal@users.noreply.github.com>
2022-06-25 10:46:01 +0530

    Update Program.cs

--- CreateBST/Program.cs
+++ CreateBST/Program.cs
@@ -10,16 +10,7 @@
             BinaryTree.AddNode(56);
             BinaryTree.AddNode(30);
             BinaryTree.AddNode(70);
-            BinaryTree.AddNode(22);
-            BinaryTree.AddNode(40);
-            BinaryTree.AddNode(11);
-            BinaryTree.AddNode(63);
-            BinaryTree.AddNode(67);
-            BinaryTree.AddNode(16);
-            BinaryTree.AddNode(60);
-            BinaryTree.AddNode(95);
-            BinaryTree.AddNode(65);
-            BinaryTree.AddNode(3);
+          
 
 
 
